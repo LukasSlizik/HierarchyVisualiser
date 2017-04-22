d041edd98ff69ba01a08953c773abf2563a01f3c
LukasSlizik <lukas.slizik@gmail.com>
2017-04-22 09:45:27 +0200

    File is dropped onto the Assembly Tree.

---
+++ HierarchyVisualiser/Behaviors/DragAndDropFileOnTreeViewBehavior.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HierarchyVisualiser.Behaviors
+{
+    public static class DragAndDropFileViewBehavior
+    {
+        #region IsDraggableProperty
+
+        /// <summary>
+        /// Set this property to allow Drag and Drop of .NET Assemblies in the Assembly Tree.
+        /// </summary>
+        public static readonly DependencyProperty CanFileBeDroppedProperty = DependencyProperty.RegisterAttached(
+                                                                                                    "CanFileBeDropped",
+                                                                                                    typeof(bool),
+                                                                                                    typeof(DragAndDropFileViewBehavior),
+                                                                                                    new PropertyMetadata(false, OnCanFileBeDropped));
+
+        public static bool GetCanFileBeDropped(TreeView el)
+        {
+            return (bool)el.GetValue(CanFileBeDroppedProperty);
+        }
+
+        public static void SetCanFileBeDropped(TreeView el, bool value)
+        {
+            el.SetValue(CanFileBeDroppedProperty, value);
+        }
+
+        #endregion
+
+        private static void OnCanFileBeDropped(DependencyObject d, DependencyPropertyChangedEventArgs args)
+        {
+            var el = (UIElement)d;
+            UnregisterEventHandlers(el);
+
+            if ((bool)args.NewValue)
+                RegisterEventHandlers(el);
+        }
+
+        private static void RegisterEventHandlers(UIElement d)
+        {
+            d.AllowDrop = true;
+            d.Drop += OnDrop;
+        }
+
+        private static void UnregisterEventHandlers(UIElement d)
+        {
+            d.AllowDrop = false;
+            d.Drop -= OnDrop;
+        }
+
+        /// <summary>
+        /// File is dropped onto the Assembly Tree.
+        /// </summary>
+        private static void OnDrop(object sender, DragEventArgs args)
+        {
+            if (args.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])args.Data.GetData(DataFormats.FileDrop);
+            }
+        }
+
+    }
+}
