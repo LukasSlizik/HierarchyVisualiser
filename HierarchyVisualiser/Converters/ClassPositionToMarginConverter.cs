b005cf9f613bb3d9bccd8b480815e34f1d530619
LukasSlizik <lukas.slizik@gmail.com>
2017-09-17 10:42:36 +0200

    ClassViews - ItemsPanel Template changed from Canvas to Grid. Positions are set throug setting the margin.

---
+++ HierarchyVisualiser/Converters/ClassPositionToMarginConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HierarchyVisualiser.Converters
+{
+    /// <summary>
+    /// Converts the Position of the Class to the Thickness Object (Margin)
+    /// </summary>
+    public class ClassPositionToMarginConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var x = (double)values[0];
+            var y = (double)values[1];
+
+            return new Thickness(x, y, 0, 0);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
