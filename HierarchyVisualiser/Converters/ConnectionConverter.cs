52dad79d4478db4444aac3575c8b2fddb21aa513
LukasSlizik <lukas.slizik@gmail.com>
2017-10-21 16:40:47 +0200

    Refactorings: Unused Code removed, Comments.

--- HierarchyVisualiser/Converters/ConnectionConverter.cs
+++ HierarchyVisualiser/Converters/ConnectionConverter.cs
@@ -5,6 +5,9 @@ using System.Windows.Media;
 
 namespace HierarchyVisualiser.Converters
 {
+    /// <summary>
+    /// Converts the positions and dimensions of two Thumbs into Geometry Data of the connection line.
+    /// </summary>
     class ConnectionConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
@@ -23,33 +26,3 @@ namespace HierarchyVisualiser.Converters
         }
     }
 }
-
-//var childX = (double)values[0];
-//var childY = (double)values[1];
-//var childWidth = (double)values[2];
-//var parentVm = (ClassViewModel)values[3];
-
-//var startPositionX = childX + (childWidth / 2);
-//var startPositionY = childY;
-
-//            if (parentVm != null)
-//            {
-//                try
-//                {
-//                    var parentX = (double)values[4];
-//var parentY = (double)values[5];
-//var parentWidth = (double)values[6];
-//var parentHeight = (double)values[7];
-
-//var endPositionX = parentX + (parentWidth / 2);
-//var endPositionY = parentY + parentHeight;
-
-//                    return new LineGeometry(new System.Windows.Point(startPositionX, startPositionY), new System.Windows.Point(endPositionX, endPositionY));
-//                }
-//                catch
-//                {
-//                    return string.Empty;
-//                }
-//            }
-
-//            return string.Empty;
