3c5eb97dac501c063c04321f3a59bb32db82221f
LukasSlizik <lukas.slizik@gmail.com>
2017-11-05 10:39:43 +0100

    Broken ConnectionLine

--- HierarchyVisualiser/Converters/ThumbsPosititionToConnectionConverter.cs
+++ HierarchyVisualiser/Converters/ThumbsPosititionToConnectionConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
 
@@ -12,6 +13,7 @@ namespace HierarchyVisualiser.Converters
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
+            // convert input
             var startX = (double)values[0];
             var startY = (double)values[1];
             var startWidth = (double)values[2];
@@ -21,7 +23,22 @@ namespace HierarchyVisualiser.Converters
             var endWidth = (double)values[6];
             var endHeight = (double)values[7];
 
-            return new LineGeometry(new System.Windows.Point(startX + (startWidth / 2), startY), new System.Windows.Point(endX + (endWidth / 2), endY + endHeight));
+            // calculate positions
+            var startXMiddle = startX + (startWidth / 2);
+            var endXMiddle = endX + (endWidth / 2);
+            var middleBetweenStartAndEnd = (startY + (endY + endHeight)) / 2;
+
+            // calculate points for the PathGeometry
+            var point1 = new Point(startXMiddle, startY);
+            var point2 = new Point(startXMiddle, middleBetweenStartAndEnd);
+            var point3 = new Point(endXMiddle, middleBetweenStartAndEnd);
+            var point4 = new Point(endXMiddle, endY + endHeight);
+
+            var segments = new[] { new LineSegment(point2, true), new LineSegment(point3, true), new LineSegment(point4, true) };
+            var myPathFigure = new PathFigure(point1, segments, false);
+
+            PathGeometry myPathGeometry = new PathGeometry(new[] { myPathFigure });
+            return myPathGeometry;
         }
 
         public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
